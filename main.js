e810990dea957f0d81ef95a46641952e0928d625
Srinjay-JavaScript <71552411+Srinjay-JavaScript@users.noreply.github.com>
2021-04-22 18:02:52 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var rightWristY = 0;
 var leftWristX = 0;
 var rightWristX = 0;
 var ScoreLeftWrist = 0;
-
+var ScoreRightWrist =0;
 function preload()
 {
  song = loadSound("music.mp3");
@@ -20,11 +20,39 @@ function setup(){
 function draw(){
     image(video, 0, 0, 400, 400);
 
-    fill("#FF000");
-    stroke("#FF000");
+    fill("red");
+    stroke("red");
+   
+    if (ScoreRightWrist > 0.2){
+    circle(rightWristX-50, rightWristY-50, 20);
+    if(rightWristY >= 0 && rightWristY <= 100)
+    {
+        document.getElementById("output_speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5);
+    }
+    else if(rightWristY >= 100 && rightWristY <= 200)
+    {
+        document.getElementById("output_speed").innerHTML = "Speed = 1x";
+        song.rate(1);
+    }
+    else if(rightWristY >= 200 && rightWristY <= 300)
+    {
+        document.getElementById("output_speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5);
+    }
+    else if(rightWristY >= 300 && rightWristY <= 400)
+    {
+        document.getElementById("output_speed").innerHTML = "Speed = 2x";
+        song.rate(2);
+    }
+    else{
+        document.getElementById("output_speed").innerHTML = "Speed = 2.5x";
+        song.rate(2.5); 
+    }
+}
 if (ScoreLeftWrist > 0.2)
 {
-    circle(leftWristX, leftWristY, 20);
+    circle(leftWristX-150, leftWristY-50, 20);
     NumberLeftWrist = Number(leftWristY);
     floored_number = floor(NumberLeftWrist);
     volume = floored_number / 500;
@@ -49,6 +77,8 @@ function gotPoses(results)
         leftWristY = results[0].pose.leftWrist.y;
         rightWristY = results[0].pose.rightWrist.y;
         ScoreLeftWrist = results[0].pose.keypoints[9].score;
+        ScoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log("Score Of RightWrist = " + ScoreRightWrist + " Score Of LeftWrist = " + ScoreLeftWrist);
 
         rightWristX = results[0].pose.rightWrist.x;
         leftWristX = results[0].pose.leftWrist.x;
